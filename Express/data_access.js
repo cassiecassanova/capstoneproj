0aac6f00c7330b9501e34b6dec2c616151e5c69d
alexliew42 <alexliew42@gmail.com>
2024-02-08 13:05:17 -0500

    added first working route to data_access.js

--- Express/data_access.js
+++ Express/data_access.js
@@ -1,10 +1,35 @@
-let products = {}; //In-memory database
-products[6] = { "name": "Dog Brush", "price": 5.99, "description": "A brush perfect for dog hair.", "animal_category": "dog", "popularity": 0.7, "id": 1 }
+// mongodb client driver
+const {MongoClient} = require('mongodb');
+var url = "mongodb://127.0.0.1:27017/";
+const client = new MongoClient(url);
+const dbName = 'fluff';
+const collectionName = "fluff";
 
-module.exports.findProduct = function(id) {
-  return books[isbn];
-};
+module.exports.call = async function call(operation, parameters, callback) {
+    await client.connect();
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
 
-module.exports.updateProduct = function(id, product) {
-  products[id] = product;
-};
+    switch(operation) {
+        case "findAllProducts":
+            const products = await collection.find({}).toArray();
+            console.log('hello')
+            console.log(products)
+            callback({ products });
+            break;
+    }
+  console.log("call complete: " + operation);
+  client.close();
+  return "call complete";
+}
+
+// let products = {}; //In-memory database
+// products[6] = { "name": "Dog Brush", "price": 5.99, "description": "A brush perfect for dog hair.", "animal_category": "dog", "popularity": 0.7, "id": 1 }
+
+// module.exports.findProduct = function(id) {
+//   return products[id];
+// };
+
+// module.exports.updateProduct = function(id, product) {
+//   products[id] = product;
+// };
--- Express/index.js
+++ Express/index.js
@@ -3,20 +3,33 @@ const dao = require("./data_access");
 const app = express();
 app.use(express.json());
 
-app.get("/product/:id", function(req, res) {
-    let product = dao.findProduct(req.params.isbn);
+// app.get("/product/:id", function(req, res) {
+//     let product = dao.findProduct(req.params.id);
   
-    if (product === undefined) {
-      res.statusCode = 404;
-      res.end();
-    } else {
-      res.send(product);
-    }
-  });
+//     if (product === undefined) {
+//       res.statusCode = 404;
+//       res.end();
+//     } else {
+//       res.send(product);
+//     }
+//   });
+
 app.get("/", function(req, res) {
-    
+    console.log(res)
+    res.send("hello")
 });
 
-  const port = 3001;
-  console.log("server starting on port: " + port );
-  app.listen(port);
+app.get("/products", function(req, res) {
+    dao.call('findAllProducts', {}, (result) => {
+        if (result !== undefined) {
+            res.send(result)
+        } else {
+            res.statusCode = 404;
+            res.end()
+        }
+    })
+})
+
+const port = 3001;
+console.log("server starting on port: " + port );
+app.listen(port);
